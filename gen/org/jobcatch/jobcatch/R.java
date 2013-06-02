d0893724301a64e56a3671ce85df10fc4f78870c
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-06-02 08:25:29 -0400

    web form

--- gen/org/jobcatch/jobcatch/R.java
+++ gen/org/jobcatch/jobcatch/R.java
@@ -26,9 +26,10 @@ public final class R {
         public static final int like=0x7f020002;
     }
     public static final class id {
-        public static final int ImageButton_dislike=0x7f080000;
-        public static final int ImageButton_like=0x7f080001;
-        public static final int action_settings=0x7f080002;
+        public static final int ImageButton_dislike=0x7f080001;
+        public static final int ImageButton_like=0x7f080002;
+        public static final int action_settings=0x7f080003;
+        public static final int indeed_job=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
